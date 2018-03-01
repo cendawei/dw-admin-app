f07aab1aaada140ff6b50be81bb79a0a367402be
cendawei <280842458@qq.com>
2018-03-01 22:43:43 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by cendawei on 2017/5/3.
+ */
+module.exports = {
+    'description': 'this is an admin-sys app generator which contain technical stack: express+sequelize+mysql+gulp+sass+sprite+webpack+es6+babel+adminlte+vue',
+    'author': 'cendawei',
+    'usage': 'dw-admin-app -d ***',
+    'dependence': 'node',
+    'installation': 'npm install -g dw-admin-app'
+}
